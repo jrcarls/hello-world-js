a10b2c54f26390d1d899c164df94ec80f08ab249
Jean Reis <jeanreis.developer@gmail.com>
2026-03-26 13:12:45 -0300

    ajuste na console log da variavel

--- cap1/umGiroPeloJavaScript.js
+++ cap1/umGiroPeloJavaScript.js
@@ -2,25 +2,25 @@ x = 0; // Declaração de váriavel modo não estrito (sloppy mode) é meio que 
 console.log(x);
 
 x = 1; // Número
-console.log(typeof x);
+console.log(x, typeof x);
 
 x = 0.01; // Números podem ser inteiros ou reais
-console.log(typeof x);
+console.log(x, typeof x);
 
 x = "Olá, Mundo!"; // também pode ser usado '' (Apóstrofos, mas é melhor usar aspas mesmo)
-console.log(typeof x);
+console.log(x, typeof x);
 
 x = true;
-console.log(typeof x);
+console.log(x, typeof x);
 
 x = false;
-console.log(typeof x);
+console.log(x, typeof x);
 
 x = null; //null é valor especial, significa "nenhum valor" semelhante ao NULL do python
-console.log(typeof x); // é do tipo object, porém sem nenhum valor
+console.log(x, typeof x); // é do tipo object, porém sem nenhum valor
 
 x = undefined; // é um valor especial assim como null
-console.log(typeof x); // o tipo é undefined, não assume nenhum tipo real por padrão
+console.log(x, typeof x); // o tipo é undefined, não assume nenhum tipo real por padrão
 
 console.log("==========");
 
